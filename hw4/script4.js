d5fc66932c94c5256783dc814f366be1e999e53c
Tatyana0808 <tas0808@gmail.com>
2023-09-15 12:14:42 +0300

    выполнила 4 задания

--- hw4/script4.js
+++ hw4/script4.js
@@ -3,3 +3,19 @@ let i = 2
  while (i) {
     console.log('Привет');
  }
+
+//Задание 2
+let i = 1;
+do {
+    console.log(i);
+    i++;
+} while (i<6);
+
+//Задание 3
+let i = 7;
+do {
+    console.log(i);
+    i++;
+} while (i<23);
+
+//Задание 4 
