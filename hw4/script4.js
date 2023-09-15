6667c0d53306de3e63e69a28e11cb3637bfa2d7e
Tatyana0808 <tas0808@gmail.com>
2023-09-15 20:22:02 +0300

    Сделала 4 и 5 задачи

--- hw4/script4.js
+++ hw4/script4.js
@@ -1,21 +1,44 @@
 //Задание 1
-//let i = 2
-// while (i) {
- //   console.log('Привет');
+//let i = 1
+ //while (i <=) {
+ //  console.log('Привет');
  //}
 
 //Задание 2
 //let i = 1;
 //do {
-   // console.log(i);
-   // i++;
+ //   console.log(i);
+  //  i++;
 //} while (i<6);
 
 //Задание 3
-let i = 7;
-do {
-    console.log(i);
-    i++;
-} while (i<23);
+//let i = 7;
+//do {
+  //  console.log(i);
+  //  i++;
+//} while (i<23);
 
 //Задание 4 
+
+//const obj = {
+   // 'Коля': 200,
+    //'Вася': 300,
+   //'Петя': 400,
+
+//};
+//for (key in obj) {
+ // if (obj[key]){
+ //   console.log('${key}-зарплата${obj[key]}долларов');
+
+ // }
+//}
+
+//Задание 5
+//n = 1000;
+//num = 0;
+//while (n>=50)
+//{
+//n=n/2;
+//num++;
+//}
+//console.log(num); 
