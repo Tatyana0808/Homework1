c49d65c9b950a639170e407fadb2fe60586e9233
Tatyana0808 <tas0808@gmail.com>
2023-09-15 12:22:49 +0300

    4 з

--- hw4/script4.js
+++ hw4/script4.js
@@ -1,15 +1,15 @@
 //Задание 1
-let i = 2
- while (i) {
-    console.log('Привет');
- }
+//let i = 2
+// while (i) {
+ //   console.log('Привет');
+ //}
 
 //Задание 2
-let i = 1;
-do {
-    console.log(i);
-    i++;
-} while (i<6);
+//let i = 1;
+//do {
+   // console.log(i);
+   // i++;
+//} while (i<6);
 
 //Задание 3
 let i = 7;
