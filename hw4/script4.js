aad4d1f7ee2022669b9057602c1b6150c5b769a8
Tatyana0808 <tas0808@gmail.com>
2023-09-17 18:46:00 +0300

    Дорешала 6 задачу и поправила остальные

--- hw4/script4.js
+++ hw4/script4.js
@@ -1,44 +1,54 @@
 //Задание 1
-//let i = 1
- //while (i <=) {
- //  console.log('Привет');
- //}
+let i = 0;
+while( i < 2 ) {
+ console.log ("Привет");
+ i++;
+}
 
 //Задание 2
-//let i = 1;
-//do {
- //   console.log(i);
-  //  i++;
-//} while (i<6);
+let c = 1;
+do {
+   console.log (c);
+    c++;
+} while (c<6);
 
 //Задание 3
-//let i = 7;
-//do {
-  //  console.log(i);
-  //  i++;
-//} while (i<23);
+let b = 7;
+do {
+    console.log (b);
+   b++;
+} while (b<23);
 
 //Задание 4 
 
-//const obj = {
-   // 'Коля': 200,
-    //'Вася': 300,
-   //'Петя': 400,
+const obj = {
+    'Коля': 200,
+    'Вася': 300,
+    'Петя': 400,
 
-//};
-//for (key in obj) {
- // if (obj[key]){
- //   console.log('${key}-зарплата${obj[key]}долларов');
+};
+for (key in obj) {
+ if (obj[key]){
+   console.log(`${key} -зарплата${obj[key]} долларов`);
 
- // }
-//}
+  }
+}
 
 //Задание 5
-//n = 1000;
-//num = 0;
-//while (n>=50)
-//{
-//n=n/2;
-//num++;
-//}
-//console.log(num); 
+n = 1000;
+num = 0;
+while (n>=50)
+{
+n=n/2;
+num++;
+}
+console.log(num);
+
+//Задание 6
+let friday = 1;
+do{
+   console.log(`Сегодня пятница, ${friday} число.Необходимо подготовить отчет`);
+   friday +=7;
+
+
+}while (friday <= 31);
