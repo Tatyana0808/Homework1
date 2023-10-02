6e322c381f3c3933412767a4d8831ce845ba5259
Tatyana0808 <tas0808@gmail.com>
2023-10-02 14:23:37 +0300

    подключила js 8

--- hw8/skript8.js
+++ hw8/skript8.js
@@ -19,9 +19,23 @@ const people = [
 //  { name: 'Глеб', age: 29 },
 //  { name: 'Оксана', age: 47 }
 // ]
+const people = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+ ];
 
-
-
+ people.sort(function (a, b) {
+	if (a.age > b.age) {
+    return 1;
+  }
+  if (a.age < b.age) {
+    return -1;
+  }
+  
+  return 0;
+});
 
 
 
