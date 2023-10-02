2eb37cd75219dc8dd4778d96ecbb6f8b155963dd
Tatyana0808 <tas0808@gmail.com>
2023-10-02 14:58:33 +0300

    решила 5 задание

--- hw8/skript8.js
+++ hw8/skript8.js
@@ -231,3 +231,20 @@ function sayHi (name) {
 
 // Нужно изменить код ниже:
 delayForSecond(sayHi('Глеб'))*/
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if (cb) {
+            cb();
+        }
+
+    }, 1000)
+}
+
+
+function sayHi(name) {
+    console.log(`Привет, ${name}!`);
+}
+
+
+delayForSecond(() => sayHi('Глеб'));
